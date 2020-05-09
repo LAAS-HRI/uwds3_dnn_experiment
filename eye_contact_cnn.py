93b5cf42714b5658bf73d35cfad0837a13507004
Yoan Sallami <ysallami@laas.fr>
2020-05-09 20:20:01 +0200

    finish eye contact detector

--- eye_contact_cnn.py
+++ eye_contact_cnn.py
@@ -1,7 +1,10 @@
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.layers import Input
 from keras.models import Model, load_model
-from keras.losses import categorical_crossentropy
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.losses import binary_crossentropy
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.regularizers import l2
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 
 
@@ -13,40 +16,60 @@ def eye_contact_cnn(input_shape):
     input = Input(input_shape)
 
     conv1 = Conv2D(20, (3, 3), padding="same",
+                   kernel_regularizer=l2(1e-4),
                    activation="relu")(input)
 
-    conv2 = Conv2D(20, (3, 3), padding="same",
-                   activation="relu")(conv1)
+    dropout1 = Dropout(0.5)(conv1)
 
-    pooling1 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv2 = Conv2D(20, (3, 3), padding="same",
+                   kernel_regularizer=l2(1e-4),
+                   activation="relu")(dropout1)
+
+    dropout2 = Dropout(0.5)(conv2)
+
+    pooling1 = MaxPooling2D(pool_size=(2, 2))(dropout2)
 
     conv3 = Conv2D(20, (3, 3), padding="same",
+                   kernel_regularizer=l2(1e-4),
                    activation="relu")(pooling1)
 
-    conv4 = Conv2D(20, (3, 3), padding="same",
-                   activation="relu")(conv3)
+    dropout3 = Dropout(0.5)(conv3)
 
-    pooling2 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    conv4 = Conv2D(20, (3, 3), padding="same",
+                   kernel_regularizer=l2(1e-4),
+                   activation="relu")(dropout3)
+
+    dropout4 = Dropout(0.5)(conv4)
+
+    pooling2 = MaxPooling2D(pool_size=(2, 2))(dropout4)
 
     flatten = Flatten()(pooling2)
 
-    dense1 = Dense(512, activation="relu")(flatten)
+    dense1 = Dense(512, activation="relu", kernel_regularizer=l2(1e-4))(flatten)
 
-    dropout = Dropout(0.5)(dense1)
+    dropout5 = Dropout(0.5)(dense1)
 
-    output = Dense(2, activation="softmax")(dropout)
+    dense2 = Dense(1024, activation="relu", kernel_regularizer=l2(1e-4))(dropout5)
+
+    dropout6 = Dropout(0.5)(dense2)
+
+    dense3 = Dense(1024, activation="relu", kernel_regularizer=l2(1e-4))(dropout6)
+
+    dropout7 = Dropout(0.5)(dense3)
+
+    output = Dense(1, activation="sigmoid")(dropout7)
 
     model = Model(input, output)
     model.summary()
-    model.compile(optimizer="adam", loss=categorical_crossentropy, metrics=["accuracy"])
+    model.compile(optimizer="SGD", loss=binary_crossentropy, metrics=["accuracy"])
     return model
 
 
 if __name__ == '__main__':
-    batch_size = 300
-    epochs = 400
-    nb_train_samples = 3959
-    nb_validation_samples = 2000
+    batch_size = 16
+    epochs = 200
+    nb_train_samples = 2000
+    nb_validation_samples = 600
 
     input_shape = (36, 60, 3)
     target_size = (36, 60)
@@ -57,9 +80,9 @@ if __name__ == '__main__':
     train_data_generator = ImageDataGenerator(rotation_range=0,
                                               height_shift_range=0.0,
                                               width_shift_range=0.0,
-                                              shear_range=0.05,
-                                              zoom_range=0.05,
-                                              horizontal_flip=True,
+                                              shear_range=0.0,
+                                              zoom_range=0.0,
+                                              horizontal_flip=False,
                                               vertical_flip=True,
                                               rescale=1./255,
                                               fill_mode="nearest")
@@ -69,25 +92,26 @@ if __name__ == '__main__':
     train_generator = train_data_generator.flow_from_directory(eye_contact_train_data_path,
                                                                target_size=target_size,
                                                                batch_size=batch_size,
-                                                               class_mode="categorical")
+                                                               class_mode="binary")
 
     validation_generator = test_data_generator.flow_from_directory(eye_contact_val_data_path,
                                                                    target_size=target_size,
                                                                    batch_size=batch_size,
-                                                                   class_mode="categorical")
+                                                                   class_mode="binary")
 
     model = eye_contact_cnn(input_shape)
 
     board = TensorBoard(log_dir="./tensorboard", histogram_freq=0, write_graph=True, write_images=True)
-    early_stop = EarlyStopping(monitor='val_acc', patience=7, verbose=1, mode='max')
-    mcp_save = ModelCheckpoint("./model.h5", save_best_only=True, monitor='val_acc', verbose=1, mode='max')
+    early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='min')
+    reduce = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+    mcp_save = ModelCheckpoint("./model.h5", save_best_only=True, monitor='val_loss', verbose=1, mode='min')
 
     model.fit_generator(train_generator,
                         steps_per_epoch=nb_train_samples // batch_size,
                         epochs=epochs,
                         validation_data=validation_generator,
                         validation_steps=nb_validation_samples // batch_size,
-                        callbacks=[early_stop, mcp_save, board])
+                        callbacks=[early_stop, mcp_save, board, reduce])
 
     best_model = load_model("./model.h5")
     model_json = best_model.to_json()
--- test_keras_opencv_dnn.py
+++ test_keras_opencv_dnn.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import argparse
 
 
@@ -19,26 +20,58 @@ def parse_args():
         required=False,
         default="exported_model/model.pbtxt",
         help="Tensorflow pbtxt file")
+    parser.add_argument(
+        "--image",
+        type=str,
+        required=False,
+        default="test.png",
+        help="Test image")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-
+    print("Loading model...")
     model = cv2.dnn.readNetFromTensorflow(args.weigths, args.model)
+    print("Model loaded, trying basic inference:")
+    bgr_img = cv2.imread(args.image)
 
-    img = cv2.imread('test.png')
+    print("Input shape: {}".format(bgr_img.shape))
 
-    print("Input shape: {}".format(img.shape))
+    blob = cv2.dnn.blobFromImage(bgr_img,
+                                 1.0/255,
+                                 bgr_img.shape[:2],
+                                 (0, 0, 0),
+                                 swapRB=True,
+                                 crop=False)
 
-    normalized_img = img/255
+    print("Input tensor:")
 
-    blob = cv2.dnn.blobFromImage(normalized_img, size=img.shape[:2], swapRB=True, crop=False)
-
-    print blob
+    print(blob)
 
     model.setInput(blob)
 
-    preds = model.forward()
+    output = model.forward()
+    print("Output tensor: {}".format(output))
 
-    print ("Prediction: {}".format(preds))
+    print("Test batch inference:")
+
+    imgs = []
+
+    imgs.append(bgr_img)
+    imgs.append(bgr_img)
+
+    print("Batch length {}".format(len(imgs)))
+
+    print("Input tensor:")
+
+    blob2 = cv2.dnn.blobFromImages(imgs,
+                                   1.0/255,
+                                   bgr_img.shape[:2],
+                                   (0, 0, 0),
+                                   swapRB=True,
+                                   crop=False)
+    print blob2
+    model.setInput(blob2)
+    output = model.forward()
+    print("Output tensor: {}".format(output))
