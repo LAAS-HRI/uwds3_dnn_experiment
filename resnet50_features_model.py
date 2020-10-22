026ebcbef8238d16ae07dd3d933395672ba076b4
Yoan Sallami <ysallami@pepperadd>
2020-10-22 15:03:09 +0200

    fix small bug and add dependencies

--- resnet50_features_model.py
+++ resnet50_features_model.py
@@ -1,11 +1,12 @@
-import keras
-import tensorflow as tf
-
-from tf.keras.applications import ResNet50
+from keras.applications.resnet50 import ResNet50
+from keras.layers import Input
+from keras.models import Model
 
 
 if __name__ == '__main__':
-    model = ResNet50(weights='imagenet', include_top=False, pooling='avg', trainable=False)
+    model = ResNet50(weights='imagenet', input_shape=(224,224,3), include_top=False, pooling='avg')
+    model.trainable = False
+    model.summary()
     model.save("resnet50_features_model.h5")
     model_json = model.to_json()
     with open("./resnet50_features_model.json", "w") as json_file:
